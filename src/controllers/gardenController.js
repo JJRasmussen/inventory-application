6106a03cde9fde5055b29d0891d563a47d1e9566
JJRasmussen <123456789+jeppe-juul@hotmail.com>
2025-05-12 14:35:04 +0200

    add overvieww routes

--- src/controllers/gardenController.js
+++ src/controllers/gardenController.js
@@ -1,9 +1,18 @@
 
 
 async function getGardenView(req, res){
-    res.render('index');
+    res.render('index', {view:'gardenView'});
+};
+async function getAllPlantBeds(req, res){
+    console.log("howdy")
+    res.render('index', {view: 'plantBeds'});
+};
+async function getAllPlants(req, res){
+    res.render('index', {view: 'plants'});
 };
 
 module.exports = {
-    getGardenView
+    getGardenView,
+    getAllPlants, 
+    getAllPlantBeds
 }
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { getGardenView } = require('../controllers/gardenController')
+const { getGardenView, getAllPlants, getAllPlantBeds  } = require('../controllers/gardenController')
 
 const indexRouter = Router();
 
 indexRouter.get('/', getGardenView);
+indexRouter.get('/plants', getAllPlants);
+indexRouter.get('/plantBeds', getAllPlantBeds);
 
 module.exports = indexRouter
